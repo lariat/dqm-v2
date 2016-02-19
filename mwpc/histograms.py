626f7ab8d875465cc235498184ffc9cd74157a74
Johnny Ho <johnnyho@uchicago.edu>
2016-02-19 11:21:15 -0600

    Fix TDC number/index bug.

--- mwpc/histograms.py
+++ mwpc/histograms.py
@@ -9,16 +9,17 @@ def hits_histograms(hits_array, col, tdc_numbers, x_range):
     bin_width = x_range[1] - x_range[0]
     range_bins = (x_range[0], x_range[-1] + bin_width)
     name = { 0 : "channel", 1 : "timing" }
-    for tdc in tdc_numbers:
+    for tdc_index in xrange(len(tdc_numbers)):
+        tdc = tdc_numbers[tdc_index]
         try:
             histogram_counts, histogram_bins = np.histogram(
-                hits_array[tdc][:, col], bins=number_bins, range=range_bins)
+                hits_array[tdc_index][:, col], bins=number_bins, range=range_bins)
             histogram_bins = histogram_bins[:-1]  # get rid of "overflow" bin
         except:
             histogram_counts = np.zeros(number_bins, dtype=np.int64)
             histogram_bins = np.arange(
                 x_range[0], x_range[-1] + bin_width, bin_width)
-        histograms[tdc] = Histogram("mwpc_tdc_{}_{}".format(tdc, name))
-        histograms[tdc].histogram_to_db(
+        histograms[tdc_index] = Histogram("mwpc_tdc_{}_{}".format(tdc, name))
+        histograms[tdc_index].histogram_to_db(
             histogram_bins, histogram_counts)
     return histograms
