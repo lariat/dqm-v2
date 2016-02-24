9d28d75e3b5c1337541f1fe2f23a3726f3d8524f
Johnny Ho <johnnyho@uchicago.edu>
2016-02-24 14:15:30 -0600

    Update.

--- dqm/views.py
+++ dqm/views.py
@@ -1,15 +1,27 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import numpy as np
 
 from flask import (
     render_template, request, jsonify, make_response, abort, redirect, url_for,
     session)
 
+from sqlalchemy import and_
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
+from redis import Redis
 
 from dqm import app
 from dqm.database import db_session
 from dqm.models import DataQualityRun, DataQualitySubRun, DataQualityLatest
+import dqm.allowed as allowed
+
+from classes import Histogram
+from metrics.binning import round_time, date_time_bins
+
+# redis client instance
+redis = Redis()
 
 def fetch_subrun(run, subrun):
     """ Fetch SubRun from database. """
@@ -38,19 +50,14 @@ def latest_run_subrun():
     date_time = Latest.date_time_updated
     return run, subrun
 
-def fetch():
-    """ Fetch Run or SubRun from database. """
+def check_run_subrun(r, sr):
+    """ Check to see if the run and sub-run numbers are OK. """
 
-    r = request.args.get('run', None)
-    sr = request.args.get('subrun', None)
+    run, subrun = None, None
 
-    run, subrun = 0, 0
-
-    if not r and not sr:
-        return "Hello, World!"
-
-    if not r and sr:
-        return "Run number not specified!"
+    if (not r and not sr) or (not r and sr):
+        print "Run number not specified!"
+        return run, subrun
 
     if r == "latest" or sr == "latest":
         latest_run, latest_subrun = latest_run_subrun()
@@ -73,32 +80,122 @@ def fetch():
             except:
                 subrun = None
 
-    if r and not sr:
-        Run = fetch_run(run)
-        if isinstance(Run, DataQualityRun):
-            print Run.date_time_updated
-        return "Run: {}".format(run)
-        return Run
-
-    if r and sr:
-        SubRun = fetch_subrun(run, subrun)
-        if isinstance(SubRun, DataQualitySubRun):
-            print SubRun.date_time
-        return "Run: {}; sub-run: {}".format(run, subrun)
-        return SubRun
+    return run, subrun
 
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
-def index():
-    return "Hello, World!"
+def home():
+    return render_template('home.html', title='Home')
 
-@app.route('/q')
-def q():
-    return fetch()
+@app.route('/json')
+def json():
+
+    run = request.args.get('run', None)
+    subrun = request.args.get('subrun', None)
+    query = request.args.get('query', None)
+
+    run, subrun = check_run_subrun(run, subrun)
+
+    if run and subrun:
+        db_row_object = fetch_subrun(run, subrun)
+    elif run and not subrun:
+        db_row_object = fetch_run(run)
+
+    if not db_row_object or not query:
+        return "NULL"
+
+    db_row_object_dict = dict(db_row_object.__dict__) 
+    db_row_object_dict.pop('_sa_instance_state', None)
+
+    if not query in db_row_object_dict:
+        return "NULL"
+
+    values = db_row_object_dict[query]
+
+    return jsonify(values=values)
+
+@app.route('/histograms')
+def histograms():
+
+    run = request.args.get('run', None)
+    subrun = request.args.get('subrun', None)
+    run, subrun = check_run_subrun(run, subrun)
+
+    db_row_object = None
+
+    if run and subrun:
+        db_row_object = fetch_subrun(run, subrun)
+    elif run and not subrun:
+        db_row_object = fetch_run(run)
+
+    if not db_row_object:
+        return "NULL"
+
+    # get list of parameters from Run or SubRun
+    db_row_object_dict = dict(db_row_object.__dict__) 
+    db_row_object_dict.pop('_sa_instance_state', None)
+    parameters = [
+        s for s in db_row_object_dict if '_histogram' in s ]
+
+    names = str(request.args.get('names', None))
+    name_list = names.split(' ')
+
+    # check to see of the histogram exists
+    for name in name_list:
+        if not any(name in s for s in parameters):
+            return "NULL"
+
+    json_data = {}
+
+    # fill json_data with histogram data
+    for name in name_list:
+        histogram = Histogram(name)
+        histogram.db_to_histogram(
+             getattr(db_row_object, name + "_histogram_bins"),
+             getattr(db_row_object, name + "_histogram_counts"),
+             getattr(db_row_object, name + "_histogram_min_bin"),
+             getattr(db_row_object, name + "_histogram_max_bin"),
+             getattr(db_row_object, name + "_histogram_bin_width"))
+        json_data[name] = {
+            'bins'   : histogram.bins.tolist(),
+            'counts' : histogram.counts.tolist(),
+            'min_bin' : histogram.min_bin,
+            'max_bin' : histogram.max_bin,
+            'bin_width' : histogram.bin_width,
+            }
+
+    return jsonify(json_data)
+
+@app.route('/metric')
+def metric():
+
+    parameter = request.args.get('parameter', None)
+
+    key_prefix = 'dqm/metric/1min/'
+
+    key = key_prefix + parameter
+
+    if redis.exists(key):
+        values = redis.lrange(key, 0, -1)
+        return jsonify(values=values)
+
+    else:
+        return "NULL"
+
+@app.route('/random')
+def random():
+    data = list(np.random.randint(5, 10, 960))
+    return jsonify(values=data)
+
+@app.route('/metrics')
+def metrics():
+    return render_template('metrics.html',
+                           title="Metrics")
 
 #@app.route('/test')
 #def test():
 #    q = request.args.get('q', None)
 #    q = str(q)
 #    return q
+
