57aa87f047b8bda7c12238d5c7870033eab1cc0b
LArIAT DQM <lariatdqm@lariat-daq01.fnal.gov>
2016-03-20 14:34:22 -0500

    update alert system on data stream page

--- dqm/static/js/alert.js
+++ dqm/static/js/alert.js
@@ -5,6 +5,7 @@ function visualAlert() {
         //$('body').effect('highlight', {color: 'red'}, 1000)
         //$('body').effect('highlight', {color: '#d9534f'}, 1000)
         $('body').effect('highlight', {color: '#c12e2a'}, 1000)
+        //$('body').effect('highlight', {color: '#f89406'}, 1000)
         alertTimeout = setTimeout(visualAlert, 1000);
     }
 }
@@ -14,6 +15,15 @@ function audioAlert() {
         $('#alarm').get(0).play();
     }
 }
+
+//function visualAlert() {
+//    $('body').effect('highlight', {color: '#c12e2a'}, 1000)
+//    alertTimeout = setTimeout(visualAlert, 1000);
+//}
+
+//function audioAlert() {
+//    $('#alarm').get(0).play();
+//}
 
 //function toggleAlertOn() {
 //    $ALERT = true;
--- dqm/static/js/data-stream-table.js
+++ dqm/static/js/data-stream-table.js
@@ -1,5 +1,19 @@
 (function() {
 
+    var caenBoard0Alert = false,
+        caenBoard1Alert = false,
+        caenBoard2Alert = false,
+        caenBoard3Alert = false,
+        caenBoard4Alert = false,
+        caenBoard5Alert = false,
+        caenBoard6Alert = false,
+        caenBoard7Alert = false,
+        caenBoard8Alert = false,
+        caenBoard9Alert = false,
+        caenBoard24Alert = false,
+        mwcAlert = false,
+        wutAlert = false;
+
     $(document).ready(function() {
         fillTable();
     });
@@ -50,107 +64,155 @@
                                                 + 'mwc_data_blocks+'
                                                 + 'wut_data_blocks'
                                                 + '&run=' + $RUN
-                                                + '&subrun=latest',
-        function (data) {
-            $("#caen-board-0-data-blocks-latest").html(JSON.stringify(data.caen_board_0_data_blocks));
-            $("#caen-board-1-data-blocks-latest").html(JSON.stringify(data.caen_board_1_data_blocks));
-            $("#caen-board-2-data-blocks-latest").html(JSON.stringify(data.caen_board_2_data_blocks));
-            $("#caen-board-3-data-blocks-latest").html(JSON.stringify(data.caen_board_3_data_blocks));
-            $("#caen-board-4-data-blocks-latest").html(JSON.stringify(data.caen_board_4_data_blocks));
-            $("#caen-board-5-data-blocks-latest").html(JSON.stringify(data.caen_board_5_data_blocks));
-            $("#caen-board-6-data-blocks-latest").html(JSON.stringify(data.caen_board_6_data_blocks));
-            $("#caen-board-7-data-blocks-latest").html(JSON.stringify(data.caen_board_7_data_blocks));
-            $("#caen-board-24-data-blocks-latest").html(JSON.stringify(data.caen_board_24_data_blocks));
-            $("#caen-board-9-data-blocks-latest").html(JSON.stringify(data.caen_board_8_data_blocks));
-            $("#caen-board-8-data-blocks-latest").html(JSON.stringify(data.caen_board_9_data_blocks));
-            $("#mwc-data-blocks-latest").html(JSON.stringify(data.mwc_data_blocks));
-            $("#wut-data-blocks-latest").html(JSON.stringify(data.wut_data_blocks));
+                                                + '&subrun=latest', updateLatestSubRun)
 
-            if (data.caen_board_0_data_blocks < 1) {
-                $("#caen-board-0").addClass('danger');
-            } else {
-                $("#caen-board-0").removeClass('danger');
-            }
+        function updateLatestSubRun(data) {
 
-            if (data.caen_board_1_data_blocks < 1) {
-                $("#caen-board-1").addClass('danger');
-            } else {
-                $("#caen-board-1").removeClass('danger');
-            }
+           $("#caen-board-0-data-blocks-latest").html(JSON.stringify(data.caen_board_0_data_blocks));
+           $("#caen-board-1-data-blocks-latest").html(JSON.stringify(data.caen_board_1_data_blocks));
+           $("#caen-board-2-data-blocks-latest").html(JSON.stringify(data.caen_board_2_data_blocks));
+           $("#caen-board-3-data-blocks-latest").html(JSON.stringify(data.caen_board_3_data_blocks));
+           $("#caen-board-4-data-blocks-latest").html(JSON.stringify(data.caen_board_4_data_blocks));
+           $("#caen-board-5-data-blocks-latest").html(JSON.stringify(data.caen_board_5_data_blocks));
+           $("#caen-board-6-data-blocks-latest").html(JSON.stringify(data.caen_board_6_data_blocks));
+           $("#caen-board-7-data-blocks-latest").html(JSON.stringify(data.caen_board_7_data_blocks));
+           $("#caen-board-24-data-blocks-latest").html(JSON.stringify(data.caen_board_24_data_blocks));
+           $("#caen-board-9-data-blocks-latest").html(JSON.stringify(data.caen_board_8_data_blocks));
+           $("#caen-board-8-data-blocks-latest").html(JSON.stringify(data.caen_board_9_data_blocks));
+           $("#mwc-data-blocks-latest").html(JSON.stringify(data.mwc_data_blocks));
+           $("#wut-data-blocks-latest").html(JSON.stringify(data.wut_data_blocks));
 
-            if (data.caen_board_2_data_blocks < 1) {
-                $("#caen-board-2").addClass('danger');
-            } else {
-                $("#caen-board-2").removeClass('danger');
-            }
+           if (data.caen_board_0_data_blocks < 1) {
+               $("#caen-board-0").addClass('danger');
+               caenBoard0Alert = true;
+           } else {
+               $("#caen-board-0").removeClass('danger');
+               caenBoard0Alert = false;
+           }
 
-            if (data.caen_board_3_data_blocks < 1) {
-                $("#caen-board-3").addClass('danger');
-            } else {
-                $("#caen-board-3").removeClass('danger');
-            }
+           if (data.caen_board_1_data_blocks < 1) {
+               $("#caen-board-1").addClass('danger');
+               caenBoard1Alert = true;
+           } else {
+               $("#caen-board-1").removeClass('danger');
+               caenBoard1Alert = false;
+           }
 
-            if (data.caen_board_4_data_blocks < 1) {
-                $("#caen-board-4").addClass('danger');
-            } else {
-                $("#caen-board-4").removeClass('danger');
-            }
+           if (data.caen_board_2_data_blocks < 1) {
+               $("#caen-board-2").addClass('danger');
+               caenBoard2Alert = true;
+           } else {
+               $("#caen-board-2").removeClass('danger');
+               caenBoard2Alert = false;
+           }
 
-            if (data.caen_board_5_data_blocks < 1) {
-                $("#caen-board-5").addClass('danger');
-            } else {
-                $("#caen-board-5").removeClass('danger');
-            }
+           if (data.caen_board_3_data_blocks < 1) {
+               $("#caen-board-3").addClass('danger');
+               caenBoard3Alert = true;
+           } else {
+               $("#caen-board-3").removeClass('danger');
+               caenBoard3Alert = false;
+           }
 
-            if (data.caen_board_6_data_blocks < 1) {
-                $("#caen-board-6").addClass('danger');
-            } else {
-                $("#caen-board-6").removeClass('danger');
-            }
+           if (data.caen_board_4_data_blocks < 1) {
+               $("#caen-board-4").addClass('danger');
+               caenBoard4Alert = true;
+           } else {
+               $("#caen-board-4").removeClass('danger');
+               caenBoard4Alert = false;
+           }
 
-            if (data.caen_board_7_data_blocks < 1) {
-                $("#caen-board-7").addClass('danger');
-            } else {
-                $("#caen-board-7").removeClass('danger');
-            }
+           if (data.caen_board_5_data_blocks < 1) {
+               $("#caen-board-5").addClass('danger');
+               caenBoard5Alert = true;
+           } else {
+               $("#caen-board-5").removeClass('danger');
+               caenBoard5Alert = false;
+           }
 
-            if (data.caen_board_24_data_blocks < 1) {
-                $("#caen-board-24").addClass('danger');
-            } else {
-                $("#caen-board-24").removeClass('danger');
-            }
+           if (data.caen_board_6_data_blocks < 1) {
+               $("#caen-board-6").addClass('danger');
+               caenBoard6Alert = true;
+           } else {
+               $("#caen-board-6").removeClass('danger');
+               caenBoard6Alert = false;
+           }
 
-            if (data.caen_board_8_data_blocks < 1) {
-                $("#caen-board-8").addClass('danger');
-            } else {
-                $("#caen-board-8").removeClass('danger');
-            }
+           if (data.caen_board_7_data_blocks < 1) {
+               $("#caen-board-7").addClass('danger');
+               caenBoard7Alert = true;
+           } else {
+               $("#caen-board-7").removeClass('danger');
+               caenBoard7Alert = false;
+           }
 
-            if (data.caen_board_9_data_blocks < 1) {
-                $("#caen-board-9").addClass('danger');
-            } else {
-                $("#caen-board-9").removeClass('danger');
-            }
+           if (data.caen_board_24_data_blocks < 1) {
+               $("#caen-board-24").addClass('danger');
+               caenBoard24Alert = true;
+           } else {
+               $("#caen-board-24").removeClass('danger');
+               caenBoard24Alert = false;
+           }
 
-            if (data.mwc_data_blocks < 1) {
-                $("#mwc").addClass('danger');
-                if ($UPDATE) {
-                    $ALERT = true;
-                    visualAlert();
-                    audioAlert();
-                }
-            } else {
-                $("#mwc").removeClass('danger');
-                $ALERT = false;
-            }
+           if (data.caen_board_8_data_blocks < 1) {
+               $("#caen-board-8").addClass('danger');
+               caenBoard8Alert = true;
+           } else {
+               $("#caen-board-8").removeClass('danger');
+               caenBoard8Alert = false;
+           }
 
-            //if (data.wut_data_blocks < 1) {
-            //    $("#wut").addClass('danger');
-            //} else {
-            //    $("#wut").removeClass('danger');
-            //}
+           if (data.caen_board_9_data_blocks < 1) {
+               $("#caen-board-9").addClass('danger');
+               caenBoard9Alert = true;
+           } else {
+               $("#caen-board-9").removeClass('danger');
+               caenBoard9Alert = false;
+           }
 
-        });
+           if (data.mwc_data_blocks < 1) {
+               $("#mwc").addClass('danger');
+               mwcAlert = true;
+           } else {
+               $("#mwc").removeClass('danger');
+               mwcAlert = false;
+           }
+
+           //if (data.wut_data_blocks < 1) {
+           //    $("#wut").addClass('danger');
+           //    wutAlert = true;
+           //} else {
+           //    $("#wut").removeClass('danger');
+           //    wutAlert = false;
+           //}
+
+           if ($UPDATE) {
+               if (caenBoard0Alert  ||
+                   caenBoard1Alert  ||
+                   caenBoard2Alert  ||
+                   caenBoard3Alert  ||
+                   caenBoard4Alert  ||
+                   caenBoard5Alert  ||
+                   caenBoard6Alert  ||
+                   caenBoard7Alert  ||
+                   caenBoard8Alert  ||
+                   caenBoard9Alert  ||
+                   caenBoard24Alert ||
+                   mwcAlert         ||
+                   wutAlert) {
+                   $ALERT = true;
+               }
+           }
+
+           if ($ALERT) {
+               visualAlert();
+               if ($AUDIO) {
+                   audioAlert();
+                   $AUDIO = false;
+               }
+           }
+
+        }
     }
 
     var timeout;
